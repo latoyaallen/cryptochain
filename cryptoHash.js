c266bf97e5e0c3084ab821e135b1266e616cc9cb
latoyaallen <me@tailorlabs.io>
2021-05-26 12:16:13 -0400

    Add native Node.js Crypto Module to handle encrypted data

---
+++ cryptoHash.js
@@ -0,0 +1,12 @@
+// native Node.js Crypto Module
+// provides a way of handling encrypted data
+// https://www.w3schools.com/nodejs/ref_crypto.asp
+const crypto = require('crypto');
+
+// use the ... spread operator.
+// inputs is an array
+// Will gather n number or args into the inputs array
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+const cryptoHash = (...inputs) => {};
+
+module.exports = cryptoHash;
