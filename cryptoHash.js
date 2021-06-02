f6b17806cc16ec7172ddac4f9c5427c378e5306a
latoyaallen <me@tailorlabs.io>
2021-06-02 11:36:03 -0400

    Digest the hash input

--- cryptoHash.js
+++ cryptoHash.js
@@ -7,6 +7,16 @@ const crypto = require('crypto');
 // inputs is an array
 // Will gather n number or args into the inputs array
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
-const cryptoHash = (...inputs) => {};
+const cryptoHash = (...inputs) => {
+  const hash = crypto.createHash('sha256');
+
+  // update takes a string, so we need to convert the inputs array to a string
+  hash.update(inputs.sort().join(' '));
+
+  // digest is a cryptography term.  We want to digest (get the result) of the hash
+  // Calculates the digest (result) of all of the data passed to be hashed
+  // https://nodejs.org/api/crypto.html#crypto_hash_digest_encoding
+  return hash.digest('hex');
+};
 
 module.exports = cryptoHash;
--- cryptoHash.test.js
+++ cryptoHash.test.js
@@ -4,12 +4,12 @@ describe('cryptoHash()', () => {
 
   it('generates a secure hash algorithm via SHA-256 hashed output', () => {
     expect(cryptoHash('Some data'))
-    .toEqual('3e2375dfb966b17175385dc2313d3373e04cb418c2e4194a027ed8db6bd198e8');
+    .toEqual('1fe638b478f8f0b2c2aab3dbfd3f05d6dfe2191cd7b4482241fe58567e37aef6');
   });
-  // I didn't like how the original test was written
+
   // context: use should stmts moving forward like Thoughtworks
-  it('should return the hashed output in any order when given multiple inputs', () => {
-    expect(cryptoHash('dog', 'cat', 'bird', 'tree')).toEqual('bird', 'cat', 'tree', 'dog');
+  it('should return the output as text in any order when given multiple inputs', () => {
+    expect(cryptoHash('dog', 'cat', 'bird', 'tree')).toEqual(cryptoHash('bird', 'cat', 'tree', 'dog'));
   });
 });
 
